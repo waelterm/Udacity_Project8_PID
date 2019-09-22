566d41b4a1e026d7f322a00e9e8e81d45935c8fd
waelterm <waelterm@msu.edu>
2019-09-22 16:38:50 -0700

    Code cleanup.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,23 +1,18 @@
 #include "PID.h"
 #include <iostream>
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+
+	// Initializing Error
 	p_error = 0;
 	i_error = 0;
 	d_error = 999999;
-	/**
-	 * PID Coefficients
-	 */
+
+	// Initializing PID Coefficients
 	Kp = Kp_;
 	Ki = Ki_;
 	Kd = Kd_;
@@ -25,25 +20,37 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
 }
 
 void PID::UpdateError(double cte) {
+	// At the first time step there is no d_error, because there is no previous error.
 	if (d_error == 999999) {
 		d_error = 0;
 	}
+
+	// Updating Integral Error
 	i_error += cte;
-	if (i_error > 20) 
+
+	// Setting saturation of integral error to avoid integral windup. 
+	if (i_error > 20)
 		i_error = 20;
 	if (i_error < -20)
 		i_error = -20;
+
+	// Updating Derivative Error
 	if (cte != p_error)
 		d_error = cte - p_error;
+
+	// Updating Proportional Error
 	p_error = cte;
-	std::cout << "Integral Component: " << i_error*Ki << std::endl;
-	std::cout << "Differantial Component: " << d_error*Kd << std::endl;
-	std::cout << "Proportional Component " << p_error*Kp << std::endl;
+
+	// Print Statements for Debugging
+	//std::cout << "Integral Component: " << i_error*Ki << std::endl;
+	//std::cout << "Differantial Component: " << d_error*Kd << std::endl;
+	//std::cout << "Proportional Component " << p_error*Kp << std::endl;
 
 }
 
 double PID::TotalError() {
+	// Calculating steering command
 	double steering_output = Kp * p_error + Kd * d_error + Ki * i_error;
-	return steering_output;  // TODO: Add your total error calc here!
+	return steering_output;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -18,103 +18,95 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_last_of("]");
-  if (found_null != string::npos) {
-    return "";
-  }
-  else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.find_last_of("]");
+	if (found_null != string::npos) {
+		return "";
+	}
+	else if (b1 != string::npos && b2 != string::npos) {
+		return s.substr(b1, b2 - b1 + 1);
+	}
+	return "";
 }
 
 int main() {
-  uWS::Hub h;
-  double squared_error_sum = 0.0;
-  long long number_of_cycles = 0;
-  PID pid;
-  pid.Init(0.11, 0.0012, 1.625); //0.06 0.0012,0.725
-  
-  //pid.Init(0.06, 0.0012, 0.525); //0.04 0.0003,0.4
-  //  pid.Init(0.15,0.0,2.5);
-  /**
-   * TODO: Initialize the pid variable.
-   */
+	uWS::Hub h;
+	double squared_error_sum = 0.0;
+	long long number_of_cycles = 0;
+	PID pid;
+	pid.Init(0.11, 0.0012, 1.625); //0.06 0.0012,0.725
 
-  h.onMessage([&pid, &squared_error_sum, &number_of_cycles](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
-                     uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-      auto s = hasData(string(data).substr(0, length));
+	h.onMessage([&pid, &squared_error_sum, &number_of_cycles](uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
+		uWS::OpCode opCode) {
+		// "42" at the start of the message means there's a websocket message event.
+		// The 4 signifies a websocket message
+		// The 2 signifies a websocket event
+		if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+			auto s = hasData(string(data).substr(0, length));
 
-      if (s != "") {
-        auto j = json::parse(s);
+			if (s != "") {
+				auto j = json::parse(s);
 
-        string event = j[0].get<string>();
+				string event = j[0].get<string>();
 
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<string>());
-          double speed = std::stod(j[1]["speed"].get<string>());
-          double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
-		  pid.UpdateError(cte);
-		  steer_value = -1.5*pid.TotalError();
-		  if (steer_value > 1) {
-			  steer_value = 1;
-		  }
-		  else if (steer_value < -1) {
-			  steer_value = -1;
-		  }
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
-          
-          // DEBUG
-          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-          //          << std::endl;
-          squared_error_sum += cte*cte;
-          number_of_cycles += 1;
-          // std::cout << "Average Squared Error: " << squared_error_sum/number_of_cycles <<std::endl;
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.22;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          //std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
-      } else {
-        // Manual driving
-        string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }  // end websocket message if
-  }); // end h.onMessage
+				if (event == "telemetry") {
+					// j[1] is the data JSON object
+					double cte = std::stod(j[1]["cte"].get<string>());
+					double speed = std::stod(j[1]["speed"].get<string>());
+					double angle = std::stod(j[1]["steering_angle"].get<string>());
+					double steer_value;
+					pid.UpdateError(cte);
+					steer_value = -1.5 * pid.TotalError();
+					if (steer_value > 1) {
+						steer_value = 1;
+					}
+					else if (steer_value < -1) {
+						steer_value = -1;
+					}
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+					// DEBUG
+					//std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl; 
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+					// Calculating mean-aquared-error
+					squared_error_sum += cte * cte;
+					number_of_cycles += 1;
+					// std::cout << "Average Squared Error: " << squared_error_sum/number_of_cycles <<std::endl;
 
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
-  } else {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  
-  h.run();
+					json msgJson;
+					msgJson["steering_angle"] = steer_value;
+					msgJson["throttle"] = 0.22;
+					auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+					//std::cout << msg << std::endl;
+					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				}  // end "telemetry" if
+			}
+			else {
+				// Manual driving
+				string msg = "42[\"manual\",{}]";
+				ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+			}
+		}  // end websocket message if
+	}); // end h.onMessage
+
+	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+		std::cout << "Connected!!!" << std::endl;
+	});
+
+	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+		char* message, size_t length) {
+		ws.close();
+		std::cout << "Disconnected" << std::endl;
+	});
+
+	int port = 4567;
+	if (h.listen(port)) {
+		std::cout << "Listening to port " << port << std::endl;
+	}
+	else {
+		std::cerr << "Failed to listen to port" << std::endl;
+		return -1;
+	}
+
+	h.run();
 }
